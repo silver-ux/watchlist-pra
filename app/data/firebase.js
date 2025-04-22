6a1046a8cc682166a2e239f7ee2096875b92bf7d
Shota <sh.studylab@gmail.com>
2025-04-22 21:02:45 +0900

    envの変更

--- app/data/firebase.js
+++ app/data/firebase.js
@@ -6,10 +6,10 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
+    appId: process.env.APP_ID
 };
 
 // Initialize Firebase
