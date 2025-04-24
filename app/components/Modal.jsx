08c36dc054a06c632520d512f2a39b363731027d
Shota in lab <sh.studylab@gmail.com>
2025-04-24 14:07:37 +0900

    完成ベータ

---
+++ app/components/Modal.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import db from '../data/firebase';
+import styles from './Modal.module.css'
+import { doc, deleteDoc } from "firebase/firestore";
+
+const Modal = ({ selectedMovie, setSelectedMovie }) => {
+
+    const [password, setPassword] = useState('');
+    const [show, setShow] = useState(false);
+
+    const handleModal = () => {
+        setSelectedMovie(null);
+    }
+
+    const handleDelete = async () => {
+        if (password == 'js') {
+            await deleteDoc(doc(db, 'posts', selectedMovie.id));
+            setSelectedMovie(null);
+            setPassword('');
+        } else {
+            alert('パスワードが違います。');
+            return;
+        }
+    }
+
+    const showPass = () => {
+        setShow(true);
+    }
+
+    return (
+        <div>
+            {selectedMovie && (
+                <div className={styles.back}>
+                    <div className={styles.modal}>
+                        <h2>{selectedMovie.title}</h2>
+                        {selectedMovie.genre.map((genre) => (
+                            <span key={`${selectedMovie.id} + ${genre}`} className={styles.genre}>{genre}</span>
+                        ))}
+                        <p className={styles.story}>{selectedMovie.story}</p>
+                        <span><b>{selectedMovie.actor}</b></span>
+                        <button className={styles.close} onClick={handleModal}>閉じる</button>
+                        <button className={styles.delete} onClick={showPass}>削除</button>
+                        {show && <div className={styles.password}>
+                            <p>本当に削除しますか？</p>
+                            <input autoFocus={true} autoComplete='off' placeholder='パスワードを入力してください。' value={password} onChange={(e) => setPassword(e.target.value)} type="text" />
+                            <button onClick={() => setShow(false)} className={`${styles.passBtns} ${styles.passclose}`}>閉じる</button>
+                            <button onClick={handleDelete} className={`${styles.passBtns} ${styles.del}`}>削除</button>
+                        </div>
+                        }
+                    </div>
+                </div>
+            )
+            }
+        </div >
+    )
+}
+
+export default Modal
---
+++ app/page.js
@@ -0,0 +1,52 @@
+'use client';
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import db from "./data/firebase";
+import List from "./components/List";
+import Header from "./components/Header";
+
+export default function Home() {
+
+  // firebaseからデータを取得してmovies配列に入れる
+  const [movies, setMovies] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setLoad(false)
+    }, 1000)
+
+    const fetchData = async () => {
+
+      // timestamp順に取得する
+      const onTime = query(
+        collection(db, "posts"),
+        orderBy("createdAt", "desc")
+      );
+      const snapShots = await getDocs(onTime);
+      const input = snapShots.docs.map((doc) => (
+        { id: doc.id, ...doc.data() }
+      ));
+      setMovies(input);
+
+      // リアルタイム更新
+      onSnapshot(onTime, (post) => {
+        setMovies(post.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      });
+
+    };
+
+    fetchData();
+  }, [])
+  return (
+    <>
+      <div className={`${styles.loading} ${load ? styles.show : styles.hide}`}><p>Loading...</p></div>
+      <div className={styles.page}>
+        <Header />
+        <List movies={movies} setMovies={setMovies} />
+      </div>
+    </>
+  );
+}
